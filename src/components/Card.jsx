f3b641d2bb36df063c1ed1fc3deba93c947b0035
Soeun Kim <summerkim0825@gmail.com>
2024-05-25 00:16:51 +0900

    Refactor: 미디어쿼리 수정

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -70,7 +70,6 @@ function Card() {
     points,
   } = MOCKDATA;
 
-
   return (
     <StyledCardContainer background={background}>
       <StyledCardHeader>
--- src/styles/media-queries.js
+++ src/styles/media-queries.js
@@ -1,13 +1,3 @@
-const BREAKPOINT_TABLET = 744;
-const BREAKPOINT_PC = 1920;
-const BREAKPOINT_MOBILE = 375;
+export const onTablet = '@media screen and (max-width: 768px)';
 
-export const onTablet = `@media only screen and (min-width: ${BREAKPOINT_TABLET}px) and (max-width: ${
-  BREAKPOINT_PC - 1
-}px)`;
-
-export const onPc = `@media only screen and (min-width: ${BREAKPOINT_PC}px)`;
-
-export const onMobile = `@media only screen and (min-width: ${BREAKPOINT_MOBILE}px and (max-width: ${
-  BREAKPOINT_TABLET - 1
-}px)`;
+export const onMobile = '@media screen and (max-width: 480px)';
